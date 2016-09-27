51b7d5a4f8bb145af605f9fe1e105d3bdf782a57
archcorsair <archcorsair@gmail.com>
2016-09-27 11:45:18 -0700

    Can submit messages to channels, displays current room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,14 @@
 const API_URL = 'https://api.parse.com/1/classes/messages';
 let app = {};
-let currentResults;
+let currentResults, currentRoom;
 let currentUser = window.location.search.slice(10);
 
-app.send = function(message, successCB) {
+app.send = function(message) {
   message = JSON.stringify(message);
   $.ajax({
     type: 'POST',
     url: API_URL,
+    success: function() { console.log('message sent'); },
     data: message,
     dataType: JSON
   });
@@ -26,6 +27,7 @@ app.fetch = function() {
 app.clearMessages = function() {
   let messages = $('#chats');
   messages.empty();
+  console.log('cleared!');
 };
 
 app.renderMessage = function(message, roomname = '#chats') {
@@ -37,23 +39,23 @@ app.renderMessage = function(message, roomname = '#chats') {
         <span class='text'>${message.text}</span>
       </div>
     </div>`);
-  $(`.${roomname}`).append(newMessage);
+  $(`.${roomname}`).prepend(newMessage);
   window.init();
 };
 
 app.renderRoom = function(event) {
-  // debugger;
+  currentRoom = event.target.id;
+  app.updateRoomName(currentRoom);
   app.clearMessages();
   let newRoom = $(`<div class='chatroom ${event.target.id}'></div>`);
   $('#chats').append(newRoom);
   for (let i = 0; i < currentResults.results.length; i++) {
-    let currentRoom = currentResults.results[i].roomname;
-    if (currentRoom === event.target.id) {
-      console.log('stuff is happening here');
+    let room = currentResults.results[i].roomname;
+    if (room === event.target.id) {
       app.renderMessage(currentResults.results[i], event.target.id);
     }
   }
-  console.log('rendered room with name: ', event.target.id);
+  window.init();
 };
 
 app.handleUsernameClick = function(event) {
@@ -61,7 +63,23 @@ app.handleUsernameClick = function(event) {
 };
 
 app.handleSubmit = function(event) {
-  console.log($('.text-field').val());
+  // Make sure user has joined a room before submitting
+  if (!currentRoom) {
+    return alert('Please join a room before submitting!');
+  }
+  // Make sure user input not blank
+  if (!$('.text-field').val()) {
+    return alert('Please type a message before submitting!');
+  }
+  let newSubmit = {};
+  newSubmit.roomname = currentRoom;
+  newSubmit.text = $('.text-field').val();
+  newSubmit.username = currentUser;
+  // Send message for submitting
+  console.log(newSubmit);
+  app.send(newSubmit);
+  console.log('rendering message locally!');
+  app.renderMessage(newSubmit, currentRoom);
 
 };
 
@@ -76,6 +94,7 @@ app.processResults = function(data) {
   let channelList = {};
   for (let i = 0; i < data.results.length; i++) {
     if (data.results[i].roomname) {
+      // Check for duplicate channel data
       if (!channelList.hasOwnProperty(data.results[i].roomname)) {
         channelList[data.results[i].roomname] = 1;
         let $dropDownItem = $('<li></li>');
@@ -87,11 +106,16 @@ app.processResults = function(data) {
         $channelName.attr('id', data.results[i].roomname);
         $($dropDownItem).append($channelName);
       } else {
-        console.log('channel already created, skipping!');
+        console.log('Duplicate channel, skipping!');
       }
 
     }
   }
+};
+
+app.updateRoomName = function(room) {
+  $('.currentroom').text(`Current Room: ${room}`).removeClass('hidden');
+  console.log('Joined Room: ', room);
 };
 
 app.fetch();
